63a2e6f96f3bd2a6850c041aa598ce44ba7f0814
Michael C. Main <deusprogrammer@gmail.com>
2021-06-28 23:37:22 -0500

    Added user page

--- gremlin-ui/src/routes/ManageUsers.jsx
+++ gremlin-ui/src/routes/ManageUsers.jsx
@@ -1,6 +1,28 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
+import {toast} from 'react-toastify';
+
+const CONTEXT_ROOT = "http://localhost:8888"
+
+const getOptions = () => {
+    return {
+        headers: {
+            "authorization": `Bearer ${window.localStorage.getItem("gremlin-token")}`
+        }
+    }
+}
 
 const ManageUsers = (props) => {
+    const [users, setUsers] = useState([]);
+    useEffect(async () => {
+        try {
+            let res = await axios.get(`${CONTEXT_ROOT}/users`, getOptions());
+            setUsers(res.data);
+        } catch (error) {
+            toast("Unable to retrieve puzzles", {type: "error"});
+        }
+    }, []);
+
     return (
         <div>
             <h1>User Management</h1>
@@ -9,7 +31,28 @@ const ManageUsers = (props) => {
                 <button>Create User</button>
             </div>
             <h3>Users</h3>
-            
+            <table>
+                <thead>
+                    <tr>
+                        <th>Username</th>
+                        <th>Roles</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {users.map((user) => {
+                        return (
+                            <tr>
+                                <td>
+                                    {user.username}
+                                </td>
+                                <td>
+                                    {user.roles}
+                                </td>
+                            </tr>
+                        )
+                    })}
+                </tbody>
+            </table>
         </div>
     )
 };
